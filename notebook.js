6850173daa0dad4c16aa0f5a9b854edd2aff3904
rtpa25 <pandaronit25@gmail.com>
2022-08-15 18:12:12 +0530

    feat: added readme

---
+++ notebook.js
@@ -0,0 +1,1 @@
+[{"content":"const a = 1;\nshow(<h1>Ronit</h1>);","type":"code","id":"ym4zb"},{"content":"# this is a heading\n\n1. this is an ordered list \n2. this is the secod point of the ordered list\n","type":"text","id":"zt09n"},{"content":"show(123)","type":"code","id":"vr2k2"},{"content":"# this is another text editor","type":"text","id":"q6dyh"}]
